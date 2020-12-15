0eead9cef959b0ae323c4a155bf461f5d7436bf1
DaisyGc <916842503@qq.com>
2020-12-15 11:24:12 +0800

    The PacmanDemo Commits

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMove : MonoBehaviour {
+
+	public GameObject [] waypointsGos;
+	public float speed = 0.2f;
+	private List <Vector3>waypoint=new List <Vector3>();
+	private int index =0;
+	private void Start()
+	{
+		LoadAPath(waypointsGos[Random.Range(0,4)]);
+	}
+	private void FixedUpdate()
+	{
+		if (transform.position != waypoint [index]) {
+			Vector2 temp = Vector2.MoveTowards (transform.position, waypoint [index],speed);
+			GetComponent<Rigidbody2D> ().MovePosition (temp);
+		} else {
+			index++;
+			if(index>=waypoint.Count)
+			{index=0;
+				LoadAPath(waypointsGos[Random.Range(0,4)]);
+			}
+		}
+		Vector2 dir = waypoint [index] - transform.position;
+		GetComponent<Animator> ().SetFloat ("DirX", dir.x);
+		GetComponent<Animator> ().SetFloat ("DirY", dir.y);
+	}
+	private void LoadAPath(GameObject go)
+	{
+		waypoint.Clear();
+		foreach(Transform t in go.transform)
+		{
+			waypoint.Add(t.position);
+		}
+	}
+
+}
---
+++ Assets/Scripts/JumpScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpScene : MonoBehaviour
+{
+    public void JumpVoid()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+    public void ExitTheGame()
+    {
+        Application.Quit();
+    }
+}
